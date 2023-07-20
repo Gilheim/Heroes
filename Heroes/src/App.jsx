3099632f8a41335e13d95110e23c33e4d2c43f26
Modernsapien <danielscott907@yahoo.com>
2023-07-20 12:46:39 +0100

    a

--- Heroes/src/App.jsx
+++ Heroes/src/App.jsx
@@ -1,20 +1,17 @@
-
-import { useState } from "react";
 import "./App.css";
 import * as Pages from "./pages";
 import { Header } from "./components";
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
-  return(
+  return (
     <>
       <Routes>
-        <Route path="/" element = {<Header/>}>
-          <Route index element = {<Pages.Homepage />} />
-          <Route path="/heroes" element = {<Pages.Heroespage />} />
-          <Route path="/heroes/:id" element = {<Pages.Heropage />} />
-          <Route path="/search" element = {<Pages.Searchpage />} />
+        <Route path="/" element={<Header />} >
+          <Route index element={<Pages.Homepage />} />
+          <Route path="/heroes" element={<Pages.HeroesPage />} />
+          <Route path="/heroes/:id" element={<Pages.HeroPage />} />
+          <Route path="/search" element={<Pages.SearchPage />} />
         </Route>
       </Routes>
     </>
--- Heroes/src/components/HeroGallery/index.jsx
+++ Heroes/src/components/HeroGallery/index.jsx
@@ -1,8 +1,33 @@
-import { useEffect, useState } from "react"
-import {Link} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function HeroGallery() {
-    return(
-        <h1>Hero Gallery</h1>
-    )
+export default function ShowGallery() {
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    async function displayHeroes() {
+  
+      const response = await fetch(
+        `https://superheroapi.com/api/243602908490010`
+      );
+      const data = await response.json();
+      setHeroes(data);
+      console.log(data)
+    }
+    displayHeroes();
+  }, []);
+
+  return (
+    <div className="heroes">
+      {heroes.map((hero) => {
+        return (
+          <Link to={`${hero.id}`} key={hero.id}>
+            <div className="gallery-image">
+              <img src={hero.url} alt={hero.name} />
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
