77898e855a308831cb7235a3ae243b3fa157a1d2
Gilheim <Guy.vernon@mac.com>
2023-07-20 13:55:44 +0100

    added more components

--- Heroes/src/components/HeroGallery/index.jsx
+++ Heroes/src/components/HeroGallery/index.jsx
@@ -2,7 +2,28 @@ import { useEffect, useState } from "react"
 import {Link} from "react-router-dom"
 
 export default function HeroGallery() {
+    const [shows, setShows] = useState([])
+
+    useEffect(() => {
+        async function displayShows() {
+            const response = await fetch(`https://superheroapi.com/api/243602908490010`)
+            const data = await response.json()
+            setShows(data)
+        }
+
+        displayShows()
+    }, [])
+
     return(
-        <h1>Hero Gallery</h1>
+        <div className= "shows">
+            {shows.map((show) => {
+                return (
+                <Link to={`${show.id}`} key = {show.id}>
+                    <div className = "gallery-image" >
+                        <img src={show.image.medium} />
+                    </div>
+                </Link>)
+            })}
+        </div>
     )
 }
